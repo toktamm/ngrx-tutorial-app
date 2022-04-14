c1d7e15ea193a779d6661da35488270033f3e2b1
Toktam <ttmohebbi@gmail.com>
2022-04-14 16:26:45 -0400

    add definition for reducers

--- src/app/reducers/tutorial.reducer.ts
+++ src/app/reducers/tutorial.reducer.ts
@@ -7,7 +7,8 @@ const initialState: Tutorial = {
   url: 'http://google.com'
 }
 
-//
+//reducer is what takes the incoming action and decides what to do with it
+//takes the previous state and returns a new state based on the given action and the payload (data that is coming through)
 export function reducer(state: Tutorial[] = [initialState], action: TutorialActions.Actions) {
   switch(action.type) {
     case TutorialActions.ADD_TUTORIAL:
